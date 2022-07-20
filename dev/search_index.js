5ca26d791fdeaa77e5683697eb3238b14def1d2c
Documenter.jl <documenter@juliadocs.github.io>
2022-07-20 08:38:26 +0000

    build based on b3cbe9b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpeedyExperiments","category":"page"},{"location":"#SpeedyExperiments","page":"Home","title":"SpeedyExperiments","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpeedyExperiments.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpeedyExperiments]","category":"page"}]
+}
