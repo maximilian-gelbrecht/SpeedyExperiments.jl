07a2f5a2ba474a812f2976d6891ab9d99ad90611
Max <maximilian.gelbrecht@posteo.de>
2022-07-25 15:18:50 +0200

    add gpu utilities

--- src/SpeedyExperiments.jl
+++ src/SpeedyExperiments.jl
@@ -1,5 +1,14 @@
 module SpeedyExperiments
 
-# Write your package code here.
+using CUDA 
+
+global const cuda_used = Ref(false)
+
+function __init__() # automatically called at runtime to set cuda_used
+    cuda_used[] = CUDA.functional()
+end
+
+include("gpu.jl")
+export DeviceArray, DeviceSparseArray
 
 end
---
+++ src/gpu.jl
@@ -0,0 +1,37 @@
+using Adapt
+using CUDA.CUSPARSE, SparseArrays
+
+"""
+    DeviceArray(x) 
+
+Returns a `CuArray` when CUDA is used, otherwise a regular `Array`
+
+"""
+DeviceArray(x) = cuda_used[] ? adapt(CuArray,x) : adapt(Array,x)
+
+
+"""
+    DeviceArray(x) 
+
+Returns a CUSPARSE Array when CUDA is used, otherwise a regular sparse Array
+
+"""
+DeviceSparseArray(x) = cuda_used[] ? CUDA.CUSPARSE.CuSparseMatrixCSC(x) : sparse(x)
+
+"""
+    gpuon()
+
+Manually toggle GPU use on (if available)
+"""
+function gpuon() # manually toggle GPU use on and off
+    cuda_used[] = CUDA.functional()
+end
+
+"""
+    gpuoff()
+
+Manually toggle GPU use off
+"""
+function gpuoff()
+    cuda_used[] = false
+end
